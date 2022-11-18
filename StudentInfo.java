545b2888a5a4a4da5e27ea5455fd10225b16758a
Gage Swann <94139813+thatoneswann@users.noreply.github.com>
2022-11-17 23:18:15 -0600

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,21 @@
+/**
+ * Simple program to print student information.
+ *
+ * @author Gage Swann
+ * @version 11/17/2022
+ */
+public class StudentInfo
+{
+   /**
+    * Prints student information to std output.
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main (String[] args)
+   {
+      // print student name and computer science courses
+      System.out.println("Name: Gage Swann") ;
+      System.out.println("Previous Computer Science Courses: ");
+      System.out.println("Computer Science Principles & A");
+   }
+}
